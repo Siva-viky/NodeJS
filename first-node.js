18f9919c642216b738c88670da868f573a145ea4
Siva Subramanian <sivasubramanian.s@vectone.com>
2022-01-06 16:43:10 +0530

    Added comments

--- first-node.js
+++ first-node.js
@@ -1,2 +1,6 @@
+// console.log() wll print the output to the terminal/console
+
+// fs is file server which helps create/modify/delete files
 const fs = require('fs');
+//using file server, we create a file and specify the content
 fs.writeFileSync('hello.txt', 'Hello from Node.js');
